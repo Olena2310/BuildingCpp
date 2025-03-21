77e7f6b0b0f62f38b11a01882c2a79fe43a77637
Olena2310 <SAKAL.OLENA@lnu.edu.ua>
2025-03-21 17:03:58 +0200

    Saving G-Test

--- testiing/test.cpp
+++ testiing/test.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
-
-TEST(TestCaseName, TestName) {
+#include"../Flat_rent/Building.h"
+#include"../Flat_rent/Flat.h"
+TEST(), TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
